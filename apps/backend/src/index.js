7c4e61cb27fe62cfdbf283ef73f77db0566d9a8b
Apurvborhade <apurvaborhadee@gmail.com>
2026-05-28 20:13:29 +0530

    Prometheus Service onitor

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -1,5 +1,13 @@
 import express from "express";
 import cors from "cors";
+import promBundle from "express-prom-bundle"
+
+
+const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includePath: true,
+})
+
 
 const app = express();
 const port = process.env.PORT ? Number(process.env.PORT) : 3001;
@@ -10,6 +18,8 @@ app.use(
   })
 );
 app.use(express.json({ limit: "1mb" }));
+app.use(metricsMiddleware);
+
 
 app.get("/api/health", (req, res) => {
   res.json({
